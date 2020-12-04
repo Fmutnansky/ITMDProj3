cd525a588a4b7189dfd3f8c95b89ec90b60e2191
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2020-12-03 23:09:24 -0600

    Update slidescript.js
    Making slideshow more interesting

--- js/slidescript.js
+++ js/slidescript.js
@@ -1,31 +1,23 @@
 //Automatic Slideshow
-var i = 0; // Start Point
-var images = []; // Images Array
-var time = 3000; // Time Between Switch
-
-// Image List
-images[0] = "media/cc.jpg";
-images[1] = "media/swe.jpg";
-images[2] = "media/cjhs.jpg";
-images[3] = "media/stc.jpg";
-images[4] = "media/hs.jfif";
+var images = [
+	"media/cc.jpg",
+	"media/swe.jpg",
+	"media/cjhs.jpg",
+	"media/stc.jpg",
+	"media/hs.jfif"],
+	curIndex = 0;
+    imgDuration = 4000; // Images Array
 
 // Change Image
 function changeImg() {
-  document.getElementById("slide").src = images[i];
-
-  // Check If Index Is Under Max
-  if (i < images.length - 1) {
-    // Add 1 to Index
-    i++;
-  } else {
-    // Reset Back To O
-    i = 0;
-  }
-
-  // Run function every x seconds
-  setTimeout("changeImg()", time);
+  document.getElementById('slider').className += "fadeOut";
+    setTimeout(function() {
+        document.getElementById('slider').src = images[curIndex];
+        document.getElementById('slider').className = "";
+    },1000);
+    curIndex++;
+    if (curIndex == images.length) { curIndex = 0; }
+    setTimeout(changeImg, imgDuration);
 }
 
-// Run function when page loads
 window.onload = changeImg;
